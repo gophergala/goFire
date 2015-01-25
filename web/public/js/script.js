44290329db05adaecd7e03ee92036e44dafaf03e
travjones <travjones0@gmail.com>
2015-01-25 15:02:15 -0500

    js

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -1,8 +1,6 @@
-var div = document.getElementById("addTask");
 var selectDiv = document.getElementById("milestoneSelectDiv")
 var clone = selectDiv.cloneNode(true);
 
-console.log(clone);
 function addTaskSelect() {
-    $("addTask").append(clone);
+    document.getElementById("addTask").appendChild(clone);
 }
